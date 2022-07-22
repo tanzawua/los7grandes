5d244f938893043d34d332eee208334d9fb62f7f
tanzawua <lostoxicos2324@gmail.com>
2022-07-21 23:27:02 -0500

    segundo

---
+++ paguinas/main2.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.showcase2', { delay: 500 });
+  ScrollReveal().reveal('.showcase3', { delay: 500 });
+  ScrollReveal().reveal('.showcase4', { delay: 500 });
+  
---
+++ paguinas/main3.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.social', { delay: 500 });
+  ScrollReveal().reveal('.social2', { delay: 600 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
